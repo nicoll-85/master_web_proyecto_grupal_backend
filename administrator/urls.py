f967da6a1a56a936afc1adea41d6c7bbfd2d64eb
Cristina <cristinabosasanchez@gmail.com>
2024-07-01 16:40:04 +0100

    Se añadio el endpoint que devuelve a los usuarios segun su rol

--- administrator/urls.py
+++ administrator/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
 from administrator.views.clients import ClientsViewSet
-
+from administrator.views.users import UsersViewSet
 
 router = routers.DefaultRouter()
-router.register(r'clients', ClientsViewSet)
+# router.register(r'clients', ClientsViewSet)
+router.register(r'users', UsersViewSet, basename = 'users')
 urlpatterns = router.urls
 
 
--- administrator/views/users.py
+++ administrator/views/users.py
@@ -1,18 +1,24 @@
 from http import HTTPStatus
 
+from django.contrib.auth.models import Group
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
+from authentication.models import User
+from authentication.serializers.user import UserSerializer
+
 
 class UsersViewSet(viewsets.ViewSet):
     permission_classes = [IsAuthenticated, IsAdminUser]
 
-    #TODO: DEVOLVER LA DATA DE TODOS LOS USUARIOS DE UN ROL. api/admin/users/<role>
     @action(detail = False, methods = ['get'], url_path = '(?P<role>[Client|Coach]+)')
     def get_users(self, request, role):
-        return Response(data = { 'msg': 'Not implemented yet "get_users"' }, status = HTTPStatus.NOT_IMPLEMENTED)
+        groups = Group.objects.get(name=role)
+        users = User.objects.filter(groups=groups)
+        serializer = UserSerializer(users, many=True)
+        return Response(data = serializer.data, status = HTTPStatus.OK)
 
     # TODO: DEVOLVER LA DATA DE UN USUARIO. api/admin/users/<pk>
     @action(detail = False, methods = ['get'], url_path = '(?P<pk>[1-9]+)')
