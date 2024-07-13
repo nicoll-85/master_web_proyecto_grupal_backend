0e413568b4cc7d7826f182be60283cd5c04bc708
Cristina <cristinabosasanchez@gmail.com>
2024-07-13 11:16:09 +0100

    Se añadio las rutas de coach

--- administrator/urls.py
+++ administrator/urls.py
@@ -1,9 +1,11 @@
 from rest_framework import routers
 from administrator.views.clients import ClientsViewSet
+from administrator.views.coachs import CoachViewSet
 from administrator.views.users import UsersViewSet
 
 router = routers.DefaultRouter()
 router.register(r'clients', ClientsViewSet, basename = 'clients')
+router.register(r'coachs', CoachViewSet, basename = 'coachs')
 router.register(r'users', UsersViewSet, basename = 'users')
 urlpatterns = router.urls
 
